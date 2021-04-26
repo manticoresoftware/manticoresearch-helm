ba04431301116a3788f69767d1b2dfd0bcd551d2
djklim87 <djklim87@gmail.com>
2021-04-26 17:21:12 +0300

    Sketches of Manticore worker

--- sources/manticore-worker/deploy.sh
+++ sources/manticore-worker/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BUILD_TAG=dev
+BUILD_TAG=0.0.1
 echo $BUILD_TAG
 docker build --no-cache -t registry.gitlab.com/manticoresearch/helm-charts/worker:$BUILD_TAG .
 docker push registry.gitlab.com/manticoresearch/helm-charts/worker:$BUILD_TAG
--- sources/manticore-worker/replica.php
+++ sources/manticore-worker/replica.php
@@ -1,26 +1,12 @@
 <?php
 
-$port            = getenv("MANTICORE_PORT");
-$statefulSetName = getenv("MANTICORE_STATEFUL_SET_NAME");
-$serviceName     = getenv("MANTICORE_SERVICE_NAME");
-$namespace       = getenv("NAMESPACE_NAME");
-$clusterName     = getenv("CLUSTER_NAME");
+$port        = getenv("MANTICORE_PORT");
+$clusterName = getenv("CLUSTER_NAME");
 
 if (empty($port)) {
     die("Set manticore port to environments\n");
 }
 
-
-if (empty($statefulSetName)) {
-    die("Set manticore statefulset name to environments\n");
-}
-
-if (empty($serviceName)) {
-    die("Set manticore service name to environments\n");
-}
-if (empty($namespace)) {
-    die("Set namespace name to environments\n");
-}
 
 for ($i = 0; $i <= 50; $i++) {
     $sphinxQL = new mysqli('localhost:' . $port, '', '', '');
@@ -29,6 +15,7 @@ for ($i = 0; $i <= 50; $i++) {
         break;
     }
 
+    echo "\n\nWait for searchd came alive\n";
     sleep(1);
 }
 
@@ -49,11 +36,19 @@ $output = json_decode($output, true);
 $min   = [];
 $count = 0;
 
+$mainPodIp = null;
+
 if ( ! empty($output['items'])) {
     foreach ($output['items'] as $pod) {
         if (isset($pod['metadata']['labels']['label'])
             && $pod['metadata']['labels']['label'] === 'manticore-worker') {
+
+            if ((int)substr($pod['metadata']['name'], -1) === 0) {
+                $mainPodIp = $pod['status']['podIP'];
+            }
+
             echo "\n\nPod status: " . $pod['status']['phase'] . "\n";
+
             if ($pod['status']['phase'] === 'Running') {
                 $min[] = substr(trim($pod['metadata']["name"]), -1);
                 $count++;
@@ -67,16 +62,11 @@ if ( ! empty($output['items'])) {
 
 echo "Replica hook: Pods count:" . $count . "\n";
 
-if ( ! empty($min)) {
-    $minNodeId = min($min);
-} else {
-    $minNodeId = 0;
-}
 
 if ($count > 1) {
     for ($i = 0; $i <= 5; $i++) {
         echo "Replica hook: Join cluster\n";
-        $sql = "JOIN CLUSTER pq_cluster at '" . $statefulSetName . "-" . ($minNodeId + $i) . "." . $serviceName . "." . $namespace . ".svc.cluster.local:9312'";
+        $sql = "JOIN CLUSTER $clusterName at '" . $mainPodIp . ":9312'";
         $sphinxQL->query($sql);
         echo "Replica hook: Sql query: $sql\n";
         if ($sphinxQL->error) {
@@ -85,6 +75,16 @@ if ($count > 1) {
             echo "Replica hook: Join success \n";
             break;
         }
+    }
+
+} else {
+
+    echo "Replica hook: Create new cluster\n";
+    $sql = "CREATE CLUSTER $clusterName";
+    $sphinxQL->query($sql);
+    echo "Replica hook: Sql query: $sql\n";
+    if ($sphinxQL->error) {
+        echo "Replica hook: QL error: " . $sphinxQL->error . "\n";
     }
 }
 
