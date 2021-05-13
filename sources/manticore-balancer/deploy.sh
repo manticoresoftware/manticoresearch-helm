a65fb58e8ba3bdaa3179e9a32b860dc0653521c5
djklim87 <djklim87@gmail.com>
2021-05-13 22:59:13 +0300

    Add Versioning notifications Add values description for exporter

--- sources/manticore-balancer/deploy.sh
+++ sources/manticore-balancer/deploy.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+#################################################################################################
+#                                                                                               #
+#   !!! Read about https://gitlab.com/manticoresearch/helm-charts/-/wikis/Versioning first !!!  #
+#                                                                                               #
+#################################################################################################
+
 BUILD_TAG=0.0.1
 echo $BUILD_TAG
 docker build --no-cache -t registry.gitlab.com/manticoresearch/helm-charts/balancer:$BUILD_TAG .
--- sources/manticore-worker/deploy.sh
+++ sources/manticore-worker/deploy.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+#################################################################################################
+#                                                                                               #
+#   !!! Read about https://gitlab.com/manticoresearch/helm-charts/-/wikis/Versioning first !!!  #
+#                                                                                               #
+#################################################################################################
+
+
 BUILD_TAG=0.0.1
 echo $BUILD_TAG
 docker build --no-cache -t registry.gitlab.com/manticoresearch/helm-charts/worker:$BUILD_TAG .
