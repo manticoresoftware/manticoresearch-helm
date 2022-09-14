9e9119a0482aa6c26e82532166022d5eeba62fdf
Klim Todrik <djklim87@gmail.com>
2022-09-14 20:45:56 +0300

    Helm labels (#30)
    * label fixes (non-stable)
    
    * Fix pullPolicy
    
    * Replication package 1.0.4
    
    * Use manticore replication package
    
    * Use new replication package
    
    * Update README.MD
    
    * Update values.yaml
    
    * Add startup probes

--- sources/manticore-balancer/deploy.sh
+++ sources/manticore-balancer/deploy.sh
@@ -6,7 +6,7 @@
 #                                                                                               #
 #################################################################################################
 
-BUILD_TAG=5.0.0.2
+BUILD_TAG=5.0.0.4
 echo $BUILD_TAG
 docker build --no-cache -t manticoresearch/helm-balancer:$BUILD_TAG .
 docker push manticoresearch/helm-balancer:$BUILD_TAG
--- sources/manticore-balancer/observer.php
+++ sources/manticore-balancer/observer.php
@@ -3,96 +3,116 @@
 
 use Core\Cache\Cache;
 use Core\K8s\ApiClient;
-use Core\Logger\Logger;
+use Core\K8s\Resources;
 use Core\Manticore\ManticoreConnector;
 use Core\Mutex\Locker;
+use Analog\Analog;
+use Analog\Handler\EchoConsole;
+use Core\Notifications\NotificationStub;
 
 require 'vendor/autoload.php';
-
-define("CONFIGMAP_PATH", getenv('CONFIGMAP_PATH'));
-define("BALANCER_PORT", getenv('BALANCER_PORT'));
-define("WORKER_PORT", getenv('WORKER_PORT'));
-define('WORKER_LABEL', getenv('WORKER_LABEL'));
+Analog::handler(EchoConsole::init());
 
 
-if ( ! file_exists(CONFIGMAP_PATH)) {
+$workerPort    = null;
+$balancerPort  = null;
+$clusterName   = null;
+$instance      = null;
+$configMapPath = null;
+$workerService = null;
+
+$variables = [
+    'workerPort'    => ['env' => 'WORKER_PORT', 'type' => 'int'],
+    'balancerPort'  => ['env' => 'BALANCER_PORT', 'type' => 'int'],
+    'clusterName'   => ['env' => 'CLUSTER_NAME', 'type' => 'string'],
+    'instance'      => ['env' => 'INSTANCE_LABEL', 'type' => 'string'],
+    'configMapPath' => ['env' => 'CONFIGMAP_PATH', 'type' => 'string'],
+    'workerService' => ['env' => 'WORKER_SERVICE', 'type' => 'string'],
+];
+
+
+foreach ($variables as $variable => $desc) {
+    $$variable = getenv($desc['env']);
+
+    if ($$variable === false) {
+        Analog::log($desc['env']." is not defined\n");
+        exit(1);
+    }
+
+    if ($desc['type'] === 'int') {
+        $$variable = (int)$$variable;
+    } elseif ($desc['type'] === 'bool') {
+        $$variable = (bool)$$variable;
+    }
+}
+
+
+$labels = [
+    'app.kubernetes.io/component' => 'worker',
+    'app.kubernetes.io/instance'  => $instance,
+];
+
+if ( ! file_exists($configMapPath)) {
     throw new RuntimeException("Searchd config is not mounted");
 }
 
+$cache  = new Cache();
 $locker = new Locker('observer');
 $locker->checkLock();
 
 
-$api   = new ApiClient();
-$cache = new Cache();
+$resources = new Resources(new ApiClient(), $labels, new NotificationStub());
 
-$manticoreStatefulsets = $api->getManticorePods(WORKER_LABEL);
-
-if ( ! isset($manticoreStatefulsets['items'])) {
-    Logger::log("FATAL: No response from k8s API");
-    exit(1);
-}
-
-
-$manticorePods = [];
-
-foreach ($manticoreStatefulsets['items'] as $pod) {
-    if ($pod['status']['phase'] === 'Running') {
-        $manticorePods[$pod["metadata"]['name']] = $pod['status']['podIP'];
-    }
-}
-
-if (empty($manticorePods)) {
-    Logger::log("No workers found");
+if ($resources->getActivePodsCount() === 0) {
+    Analog::log("No workers found");
     $locker->unlock();
 }
+$oldestWorker = $resources->getOldestActivePodName();
 
-$podsIps = array_values($manticorePods);
-ksort($manticorePods);
-$url = array_shift($manticorePods);
+if (empty($oldestWorker)) {
+    throw new RuntimeException("Can't find oldest worker");
+}
 
-/**
- * Get current state of indexes. For that we request first manticore node directly.
- */
-
-
-$manticore = new ManticoreConnector($url, WORKER_PORT, WORKER_LABEL, -1);
+$manticore = new ManticoreConnector($oldestWorker.'.'.$workerService, $workerPort, null, -1);
 $tables    = $manticore->getTables(false);
+$podsIps   = $resources->getPodsIp();
 
 if ($tables !== []) {
     $previousHash = $cache->get(Cache::INDEX_HASH);
-    $hash         = sha1(implode('.', $tables) . implode($podsIps));
+    $hash         = sha1(implode('.', $tables).implode($podsIps));
 
     if ($previousHash !== $hash) {
-        Logger::log("Starting recompiling config");
-        saveConfig($tables, $podsIps);
+        Analog::log("Starting recompiling config");
+        saveConfig($tables, $podsIps, $balancerPort, $configMapPath);
         $cache->store(Cache::INDEX_HASH, $hash);
     }
-
 } else {
-    Logger::log("No tables found");
+    Analog::log("No tables found");
     $locker->unlock();
 }
 
 
-function saveConfig($indexes, $nodes)
+function saveConfig($indexes, $nodes, $port, $configMapPath)
 {
-    $searchdConfig = file_get_contents(CONFIGMAP_PATH);
+    $searchdConfig = file_get_contents($configMapPath);
     $prependConfig = '';
     foreach ($indexes as $index) {
-        $prependConfig .= "\n\nindex " . $index . "\n" .
-                          "{\n" .
-                          "\ttype = distributed\n" .
-                          "\tha_strategy = roundrobin\n" .
-                          "\tagent = " . implode("|", $nodes) . "\n" .
-                          "}\n\n";
+        $prependConfig .= "\n\nindex ".$index."\n".
+            "{\n".
+            "\ttype = distributed\n".
+            "\tha_strategy = roundrobin\n".
+            "\tagent = ".implode("|", $nodes)."\n".
+            "}\n\n";
     }
 
-    file_put_contents(DIRECTORY_SEPARATOR . 'etc' .
-                      DIRECTORY_SEPARATOR . 'manticoresearch' .
-                      DIRECTORY_SEPARATOR . 'manticore.conf', $prependConfig . $searchdConfig);
+    file_put_contents(
+        DIRECTORY_SEPARATOR.'etc'.
+        DIRECTORY_SEPARATOR.'manticoresearch'.
+        DIRECTORY_SEPARATOR.'manticore.conf',
+        $prependConfig.$searchdConfig
+    );
 
-    (new ManticoreConnector('localhost', BALANCER_PORT, WORKER_LABEL, -1))->reloadIndexes();
+    (new ManticoreConnector('localhost', $port, null, -1))->reloadIndexes();
 }
 
 
