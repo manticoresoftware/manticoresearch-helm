1a39990f21e1dd7bd7b6b3d97c1c37129a61e802
Klim Todrik <djklim87@gmail.com>
2022-09-28 19:56:38 +0400

    Remove unnecessary files

--- sources/manticore-balancer/job_runner.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-while [ true ]; do
-  php observer.php &
-  sleep $OBSERVER_RUN_INTERVAL
-done
--- sources/manticore-balancer/starter.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cp -f $CONFIGMAP_PATH /etc/manticoresearch/manticore.conf
-/usr/bin/supervisord -n -c /etc/supervisord.conf
