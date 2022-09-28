34a3a11d83a6f5e57307935328bb60c633ba6d32
Klim Todrik <djklim87@gmail.com>
2022-09-28 21:36:20 +0400

    Version 5.0.2.0

--- sources/manticore-worker/deploy.sh
+++ sources/manticore-worker/deploy.sh
@@ -7,7 +7,7 @@
 #################################################################################################
 
 
-BUILD_TAG=5.0.0.4
+BUILD_TAG=5.0.2.0
 echo $BUILD_TAG
 docker build --no-cache -t manticoresearch/helm-worker:$BUILD_TAG .
 docker push manticoresearch/helm-worker:$BUILD_TAG
