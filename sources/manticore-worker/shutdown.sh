26b6ee574af7bbf068abc0f4d16141154697b98d
djklim87 <djklim87@gmail.com>
2023-07-23 18:04:51 +0200

    Sketches of several balancers support

--- sources/manticore-worker/shutdown.sh
+++ sources/manticore-worker/shutdown.sh
@@ -7,5 +7,5 @@ KUBE_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 CONTENT=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
 "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/$NAMESPACE/pods/?labelSelector=app.kubernetes.io/component=balancer&app.kubernetes.io/instance=$INSTANCE_LABEL")
 
-# TODO pass there port according values yaml
+# TODO pass there port according values yaml ($NAMESPACE)
 curl -v $(echo $CONTENT | jq '.items[].status.podIP + ":8080"')
