1c5ecb2d88fbb5b5334d680aa33983491bc0138c
Don Hardman <59067742+donhardman@users.noreply.github.com>
2024-06-06 22:23:41 +0700

    Update CLT to latest version and remove echo $? (#91)
    * Update CLT to latest version and remove echo $?
    
    * Fix CLT test and add some paths related to CLT to gitignore
    
    * Feat: Fix failed clt tests
    
    ---------
    
    Co-authored-by: djklim87 <klim@manticoresearch.com>

--- clt_tests/clt-init.sh
+++ clt_tests/clt-init.sh
@@ -1,27 +1,19 @@
 #!/usr/bin/env bash
 
-
 rm -rf k3s.yaml k3s_copy.yaml
-
 docker stop k3s
 docker stop kubectl
-
 docker rm k3s
 docker rm kubectl
-
-
 touch k3s.yaml
 docker run -d --name k3s -v $(pwd)/k3s.yaml:/etc/rancher/k3s/k3s.yaml --privileged -it rancher/k3s server
-
 sleep 30
-
 docker build -t manticoresearch/helm-test-kit:0.0.1 .
-
 cp k3s.yaml k3s_copy.yaml
-
 chmod 0777 k3s_copy.yaml
-
 K3SIP=$(docker inspect k3s -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' | head -n1)
+
+echo $K3SIP
 
 sed -i "s/127.0.0.1/${K3SIP}/g" k3s_copy.yaml
 
