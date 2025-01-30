79cd33a1e4d3372c853002a1864e523c3cdb7c0c
djklim87 <klim@manticoresearch.com>
2025-01-30 15:09:19 +0100

    Update dependencies

--- sources/manticore-worker/deploy.sh
+++ sources/manticore-worker/deploy.sh
@@ -8,5 +8,5 @@
 
 BUILD_TAG=$(cat ../../charts/manticoresearch/Chart.yaml | grep appVersion | cut -d" " -f2)
 echo $BUILD_TAG
-docker build --no-cache --platform linux/amd64 -t manticoresearch/helm-worker:$BUILD_TAG .
+docker build --no-cache --platform linux/amd64 -t manticoresearch/helm-worker:$BUILD_TAG . && \
 docker push manticoresearch/helm-worker:$BUILD_TAG
